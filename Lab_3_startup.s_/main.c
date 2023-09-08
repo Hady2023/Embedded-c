29e23b1261583ba1cd00f0b22088a5e585b239ad
Your Name <you@example.com>
2023-09-08 23:53:44 +0200

    new add

---
+++ Lab_3_startup.s_/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include"Typedef_plart_form.h"
+#define RCC_BASE   0x4002100
+#define GPIO_BASE  0x4001800
+
+#define RCC_AB2ENR  *(volatile uint32*)(RCC_BASE + 0x18)
+#define GPIO_CRH    *(volatile uint32*)(GPIO_BASE + 0x04)
+#define GPIO_ODR    *(volatile uint32*)(GPIO_BASE + 0x0c)
+
+volatile uint32  x_0 [3] ={ 1 , 2 , 3 } ; // .Data section 
+volatile const  double x_1 [3]= {1 , 2 , 3 }; //.Bss  section 
+volatile uint8  a =0 , b =0 ; 
+int main(void)
+{
+
+RCC_AB2ENR|=(1<<2);        // Enable clock
+GPIO_CRH &= 0xff0fffff;   //Enable output from 20 to 24 = 0   clear
+GPIO_CRH |= 0x00200000;   //Enable by  or with 2
+
+while(1)
+{
+	GPIO_ODR |= (1<<13); //set pin
+	for ( a =0 ; a<1000 ; a++);
+	GPIO_ODR &= ~(1<<13);  // clear pin
+	for ( b =0 ; b<1000 ; b++);
+}
+	return 0;
+}
---
+++ Lab_3_startup.s_/startup.s
@@ -0,0 +1,26 @@
+#Done by: Hady Ayman 
+#Date: 7/9/2023 
+#Titel: ///*Simple startup.s*///   
+#1:Inthlization the SP in adderss 0x08000000 by the value of stack_top 
+#2:Add the vector handler Function and use .word in each line to imporve from aligen address     
+#3:As simple as possible Reset_handler call tha _reset to baranch at main  
+#4:Use .section to divide sections as .vector and .texe  
+#5:Use thumb to To allow accepte all 16 and 32 bit instruction 
+   
+.section .vectors 
+.word _stack_top
+.word Reset_handler 
+.word Hf_handler 
+
+.section .text 
+Hf_handler:
+ b Reset_handler 
+ 
+Reset_handler:
+ b _reset
+ 
+.thumb_func 
+_reset:
+ b main 
+ b . 
+ 
